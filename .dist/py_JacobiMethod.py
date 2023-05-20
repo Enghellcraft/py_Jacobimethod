969dc57e77edb55c806ce75516d6d96ac699d044
Denise <97702569+Enghellcraft@users.noreply.github.com>
2023-05-20 15:13:57 -0300

    changes added

--- .dist/py_JacobiMethod.py
+++ .dist/py_JacobiMethod.py
@@ -7,6 +7,7 @@
 #           Para correr el programa es necesario tener instalado scipy y matplotlib. 
 
 # Imports
+import random
 import numpy as np
 import scipy.linalg as linalg 
 import matplotlib.pyplot as plt
@@ -16,40 +17,37 @@ import matplotlib.pyplot as plt
 def my_square_matrix(A):
     A = np.array(A)
     # Revisa si el numero de Columnas es igual al de Filas
-    if (A.shape[0] != A.shape[1]):
-        return False
-    else:
-        return True
+    return A.shape[0] == A.shape[1]
 
 ## Strictly dominant diagonal matrix
 def my_diagonal_strictly_dominant(A):
-    N = range(len(A))
-    # Por la longitud de la matriz, revisa si el valor diagonal es menor o igual al de los 
-    # valores de las filas correpondiente a ella, recalculando por cada fila su longitud
-    for i in N:
-        row_sum = sum(abs(A[i][j]) for j in range(len(A[i])) if j != i)
-        if (abs(A[i][i]) <= row_sum):
-            return False
-    return True
+    # Guarda en diagonal, todos los valores absolutos de la diagonalde la matriz 
+    diagonal = np.diag(np.abs(A)) 
+    # print(diagonal)
+    # Guarda en sum la sumatoria de las filas menos el valor diagonal
+    sum = np.sum(np.abs(A), axis=1) - diagonal 
+    # print(sum)
+    # Compara para cada valor diagonal si es mayor o igual al de la final a la que pertenece
+    return (np.all(diagonal >= sum))
 
 ## Checks whether the determinant of the matrix is Zero
-def my_determinant_is_zero(A):
-    if (linalg.det(A) == 0):
+def my_determinant_is_not_zero(A):
+    # print(linalg.det(A))
+    return (linalg.det(A) != 0)
+
+## Checks whether the matrix is inversible 
+def my_has_inverse(A):
+    # Se verifica su inversibilidad por determinante
+    if my_determinant_is_not_zero(A) == False:
+        print("  La matriz no es inversible")
         return False
     else:
         return True
-
-## Inverts a matrix
+    
+## Inverts the matrix
 def my_inverse(A):
-    Am = np.matrix(A)
-    # Para poder invertirse una matriz tiene por requisito ser cuadrada
-    if my_square_matrix(A) == True :
-            # Se verifica su inversibilidad por determinante
-            if (linalg.det(Am) == 0):
-                return print("  La matriz no es inversible")
-            else:
-                linalg.inv(Am)
-                
+    return linalg.inv(A)
+                 
 ## Calculates a zero vector according to the given matrix size
 def my_zero_vector(A):
     # Calcula una dimension de la matriz
@@ -60,6 +58,15 @@ def my_zero_vector(A):
                 
 ## Calculates Jacobi
 def my_jacobi(A, b, k, n):
+    # Requiere un minimo de dos pasos para observar el proceso
+    if (k <= 0):
+        k = 2
+    # No permite que las dimensiones de la matriz sean menores a 2*2
+    if (n <= 0):
+        n = 2
+    print("  La Matriz A es:")
+    print_matrix(A)
+    print("                                                                                  ")
     # Compuba que la matriz sea cuadrada
     if(my_square_matrix(A)) == False:
         print("  La matriz no es cuadrada y se sugiere no continuar")
@@ -68,81 +75,105 @@ def my_jacobi(A, b, k, n):
         if (my_diagonal_strictly_dominant(A)) == False:
             print("  La diagonal no es estrictamente dominante y puede no convergir")    
         else:
-            if (my_determinant_is_zero(A)):
+            if (my_determinant_is_not_zero(A)) == False:
                 print("La determinante de la Matriz A es cero y no se puede continuar")
             else:
                     # Calcula D como la diagonal de A
                     D = np.diag(np.diag(A))
                     # Comprueba que la diagonal sea inversible
-                    if (my_inverse(D)) == False:
+                    if (my_has_inverse(D)) == False:
+                        # Como la determinante de la Matriz A es distinta de cero, esto deberia ser siempre verdadero
+                        # Sin embargo como medida de precaución se agrega dentro de las validaciones
                         print("La matriz D no es inversible y no se puede continuar")
                     else:
                         # Calcula L como la matriz inferior estricta de A
                         L = np.tril(A, k=-1)
                         # Calcula L como la matriz superior estricta de A
                         U = np.triu(A, k=1)
-                        print("                                                                                  ")
-                        print("  La Matriz A es:")
-                        print(A)
                         print("  A = L + U + D, donde:")
                         print("  La Matriz L es:")
-                        print(L)
+                        print_matrix(L)
                         print("  La Matriz U es:")
-                        print(U)
+                        print_matrix(U)
                         print("  La Matriz D es:")
-                        print(D)
+                        Da = np.array(D)
+                        print_matrix(np.round(Da,1))
                         print("                                                                                  ")
-                        print("  La Matriz inversa de D es:")
-                        Dinv = my_inverse(D)
-                        print(Dinv)
-                        print("  El Vector b es:")
-                        print(b)
-                        print("  La Sumatoria de L + U es: ")
-                        LU = L + U
-                        print(LU)
-                        print("                                                                                  ")
-                        # Calcula el primer paso de Jacobi: H
-                        print("  Considerando: H = D^-1 * (L + U)")
-                        H = -Dinv * (LU)
-                        print(f"  H es: \n {H}")
-                        # Calcula si la norma de H es valida: || H || < 1
-                        if (linalg.norm(H) >= 1 ):
-                            print("  La norma de H es inválida para continuar: por H >= 1                            ")
-                        else:
-                            # Calcula el segundo paso de Jacobi: v
-                            print("  Considerando v = D^-1 * b")
-                            v = Dinv * b
-                            print(f"  v es: \n {v}")
+                        if (my_has_inverse(D) == True):
+                            print("  La Matriz inversa de D es:")
+                            Dinv = my_inverse(D)
+                            Dinva = np.array(Dinv)
+                            print_matrix(Dinva)
+                            print("  El Vector b es:")
+                            print(b)
+                            print("  La Sumatoria de L + U es: ")
+                            LU = L + U
+                            print_matrix(LU)
                             print("                                                                                  ")
-                            print("  Para comenzar tomaremos el vector inicial Xo:")
-                            x0 = my_zero_vector(A)
-                            print(f"  X0 es: \n {x0}")
-                            print("  Consideraremos el error como la norma de la diferencia entre X0 y X1:           ")
-                            print("  error = || x - x-1 ||                                                           ")
-                            print("  Y una tolerancia de 1e-3                                                        ")
-                            tol = 1e-3
-                            print("                                                                                  ")
-                            for i in range(k):
-                                # Calcula el vector solución de Jacobi para la iteración
-                                x = H * x0 + v
-                                print(f"  En la Iteración {i+1} el vector resultante x es: {x}, con un vector x-1: {x0}")
-                                # Verifica el error a partir de la segunda iteración
-                                if i < 0:
-                                    # Calcula el error 
-                                    e = linalg.norm(x - x0)
-                                    print(f"  El error es esta iteración {i+1} es de: {e}")
-                                # Reasigna el nuevo X0 como el X del paso anterior
-                                if (e < tol):
-                                    break
-                                x0 = x
+                            # Calcula el primer paso de Jacobi: H
+                            print("  Considerando: H = D^-1 * (L + U)")
+                            H = my_H(Dinv, L, U)
+                            print("  H es:")
+                            print_matrix(H)
+                            # Calcula si la norma de H es valida: || H ||2 < 1
+                            if (linalg.norm(H) >= 1 ):
+                                print("  La norma de H es inválida para continuar: por || H || >= 1                       ")
+                                print(f"  La norma de H es: {linalg.norm(H):.1f}")
+                            else:
+                                # Calcula el segundo paso de Jacobi: v
+                                print("  Considerando v = D^-1 * b")
+                                ba = np.array(b)
+                                v = np.dot(Dinv,b)
+                                print("  v es: ")
+                                print(v)
+                                print("                                                                                  ")
+                                print("  Para comenzar tomaremos el vector inicial Xo:")
+                                x0 = b
+                                print("  X0 es: ")
+                                print(x0)
+                                print("  Consideraremos el error como la norma de la diferencia entre X0 y X1:           ")
+                                print("  error = || x - x-1 ||                                                           ")
+                                print("  Y una tolerancia de 1e-3                                                        ")
+                                tol = 1e-3
+                                print("                                                                                  ")
+                                for i in range(k):
+                                    # Calcula el vector solución de Jacobi para la iteración
+                                    x = my_x(H, v, x0)
+                                    print(f"  En la Iteración {i+1} el vector resultante x es: {print_matrix(x)}, con un vector x-1: {print(x0)}")
+                                    # Verifica el error a partir de la segunda iteración
+                                    if i < 0:
+                                        # Calcula el error 
+                                        e = linalg.norm(x - x0)
+                                        print(f"  El error es esta iteración {i+1} es de: {e}")
+                                    # Reasigna el nuevo X0 como el X del paso anterior
+                                    if (e < tol):
+                                        break
+                                    x0 = x
 
-                            return x, e
+                                return x, e
+
+## Calculates H
+def my_H(dinv, L, U):
+    LU = L + U
+    H = -np.dot(dinv, LU)
+    return H
+
+def my_x(H, v, x0):
+    mul = np.dot(H, x0)
+    final = mul + v
+    
+## Creates a matrix with n parameter
+def my_generate_matrix(n):
+    random_list = [random.randint(0, 9) for i in range(n*n)]
+    matrix = np.reshape(random_list, (n, n))
+    return matrix   
                 
 ## Print matrix
 def print_matrix(A):
     a = np.array(A)
-    for line in a:
-        print ('  '.join(map(str, line)))
+    for row in a: 
+        # Si el valor es int lo imprime como tal, si es un float lo trunca a un decimal
+        print(*[f"{value:.1f}" if not value.is_integer() else f"{value:.0f}" for value in row])
 
 ## Print matrix graphics
 def print_matrix_graphics(x):
@@ -152,7 +183,21 @@ def print_matrix_graphics(x):
     plt.ylabel('Solución')
     plt.title('Solución del Sistema Linear usando Método LU')
     plt.show() 
-    
+  
+# Dataset
+non_square = [[1,2,3],[4,5,6]]
+
+det_zero = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+diag_not_dominant = [[1, 2, 3], [4, 1, 6], [7, 8, 1]]
+
+almost_doable_matrix = [[3, 2, 1],[1, 2, 1],[1, 2, 3]]
+
+doable_matrix = [[5, 2, 1],[1, 6, 3],[2, 3, 7]]
+
+b3 = [0, 0, 0]
+
+b2 = [0, 0]
 
 # Prints
 ## null) Task + Pres
@@ -219,29 +264,32 @@ print("*************************************************************************
 print("*                                    EJEMPLOS                                    *")
 print("**********************************************************************************")
 print("    • Se comprueba si la matriz es cuadrada:                                      ")
-
+my_jacobi(non_square, b3, 6, 3)
 print("                                                                                  ")
 print("    • Se comprueba si la diagonal es estrictamente dominante:                     ")
-
+my_jacobi(diag_not_dominant, b3, 6, 3)
 print("                                                                                  ")
 print("    • Se comprueba si la matriz tiene determinante = 0                            ")
-
+my_jacobi(det_zero, b3, 6, 3)
+print("                                                                                  ")
+print("    • Se comprueba si la norma de H > 1:                                          ") 
+my_jacobi(almost_doable_matrix, b3, 6, 3)
 print("                                                                                  ")
 print("    • Si cumple todos los requisitos, se emiten todos los valores y cálculos:     ") 
-
+my_jacobi(doable_matrix, b3, 6, 3)
 
 ## III) Comparación Extra
-""" import autograd.numpy as np
+""" import autograd.numpy as autog
 from autograd import jacobian
 
-x = np.array([5, 3], dtype=float)
+x = autog.array([5, 3], dtype=float)
 
 def cost(x):
-    return x[0]**2 / x[1] - np.log(x[1])
+    return x[0]**2 / x[1] - autog.log(x[1])
 
 jacobian_cost = jacobian(cost)
 
-jacobian_cost(np.array([x, x, x]))
+jacobian_cost(autog.array([x, x, x]))
  """
 
 ## IV) Conclusions
